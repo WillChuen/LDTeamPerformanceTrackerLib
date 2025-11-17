d5593aa3e064c197b5650daf08b38ce2fc94d869
zhongwei <1063163323@qq.com>
2025-11-17 21:15:28 +0800

    添加监控适配插件代码

---
+++ LDTeamPerformanceTrackerLib/Classes/LDTeamTrackerPlugin.swift
@@ -0,0 +1,58 @@
+//
+//  LDTeamTrackerPlugin.swift
+//  Pods-LDTeamPerformanceTrackerLib_Example
+//
+//  Created by zhongwei on 2025/11/17.
+//
+
+import Foundation
+
+/// 团队监控适配插件
+public protocol LDTeamTrackerPlugin {
+    /// 启动跟踪
+    func start(name: String,
+               operation: String)
+    /// 结束跟踪
+    func finish(name: String,
+                status: Int)
+    /// 设置跟踪器数据
+    func setTrackerData(name: String,
+                        value: Any?,
+                        forKey key: String)
+    /// 移除跟踪器数据
+    func removeTrackerData(name: String,
+                           forKey key: String)
+    /// 设置跟踪器标签
+    func setTrackerTag(name: String,
+                       value: String,
+                       forKey key: String)
+    /// 移除跟踪器标签
+    func removeTrackerTag(name: String,
+                          forKey key: String)
+    /// 开始一个跨度
+    func startSpan(name: String,
+                   operation: String,
+                   description: String)
+    /// 结束一个跨度
+    func finishSpan(name: String,
+                    operation: String,
+                    status: Int)
+    /// 设置子跨度数据
+    func setTrackerChildData(name: String,
+                             operation: String,
+                             value: Any?,
+                             forKey key: String)
+    /// 移除子跨度数据
+    func removeTrackerChildData(name: String,
+                                operation: String,
+                                forKey key: String)
+    /// 设置子跨度标签
+    func setTrackerChildTag(name: String,
+                            operation: String,
+                            value: String,
+                            forKey key: String)
+    /// 移除子跨度标签
+    func removeTrackerChildTag(name: String,
+                               operation: String,
+                               forKey key: String)
+}
---
+++ LDTeamPerformanceTrackerLib/Classes/LDTeamTrackerStatus.swift
@@ -0,0 +1,48 @@
+//
+//  LDTeamTrackerStatus.swift
+//  Pods-LDTeamPerformanceTrackerLib_Example
+//
+//  Created by zhongwei on 2025/11/17.
+//
+
+import Foundation
+
+/// 团队监控结束状态
+public enum LDTeamTrackerStatus: Int {
+    /// 未定义状态
+    case undefined = -100000
+    /// 成功
+    case ok = 0
+    /// 超时
+    case deadlineExceeded = -100001
+    /// 未认证
+    case unauthenticated = -100002
+    /// 权限不足
+    case permissionDenied = -100003
+    /// 未找到
+    case notFound = -100004
+    /// 资源耗尽
+    case resourceExhausted = -100005
+    /// 参数无效
+    case invalidArgument = -100006
+    /// 未实现
+    case unimplemented = -100007
+    /// 服务不可用
+    case unavailable = -100008
+    /// 内部错误
+    case internalError = -100009
+    /// 未知错误
+    case unknownError = -100010
+    /// 已取消
+    case cancelled = -100011
+    /// 已存在
+    case alreadyExists = -100012
+    /// 前置条件失败
+    case failedPrecondition = -100013
+    /// 操作中止
+    case aborted = -100014
+    /// 超出范围
+    case outOfRange = -100015
+    /// 数据丢失
+    case dataLoss = -100016
+}
