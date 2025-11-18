754976e24b2d7fa8c236a0caa83d4a27e3a5298c
zhongwei <1063163323@qq.com>
2025-11-18 19:48:11 +0800

    111

--- LDTeamPerformanceTrackerLib/Classes/LDTeamCommonTracker.swift
+++ LDTeamPerformanceTrackerLib/Classes/LDTeamCommonTracker.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 /// 团队通用监控适配插件
-@objc(LDTeamCommonTracker) class LDTeamCommonTracker: NSObject, LDTeamTrackerPlugin {
+@objc(LDTeamCommonTracker) public class LDTeamCommonTracker: NSObject, LDTeamTrackerPlugin {
     /// 开始一个监控
-    func start(name: String, operation: String) {
+    public func start(name: String, operation: String) {
         print("CommonTracker Start - Name: \(name), Operation: \(operation)")
     }
     /// 结束一个监控
-    func finish(name: String, status: LDTeamTrackerStatus) {
+    public func finish(name: String, status: LDTeamTrackerStatus) {
         print("CommonTracker Finish - Name: \(name), Status: \(status)")
     }
 }
